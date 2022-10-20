cfacfc84e2cce2234026419412af396b7e52f7a4
jcdavidarg <jcdavidarg@gmail.com>
2022-10-19 21:58:38 -0300

    Se crea elemento ui input y componente login.

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,29 @@
-import type { NextPage } from "next";
+import Link from "next/link";
+import Button from "../../ui/button";
+import Titulo from "../../ui/title";
+import Input from "../../ui/input";
+import style from "./index.module.css";
 
-const Login: NextPage = () => {
+const SearchPage = () => {
   return (
-    <div className={""}>
-      <h1>Login</h1>
+    <div className={style.container}>
+      <div className={style.tituloContainer}>
+        <Titulo titulo={"Ingresa tu nombre"} color="white" textAlign="center" />
+      </div>
+      <form>
+        <div className={style.inputContainer}>
+          <Input tipo="text" placeholder="Escribe aquí" />
+        </div>
+        <div className={style.buttonContainer}>
+          <Link href={"/homepage"}>
+            <a>
+              <Button displayName="Ingresar" />
+            </a>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default Login;
+export default SearchPage;
