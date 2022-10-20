16d00262e04df43d3e289fb42e695cf30e49a8c5
Elias Kanneman <elias_gk1@hotmail.com>
2022-10-20 11:21:00 -0300

    I changed the search style

--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -1,6 +1,9 @@
+import Search from "../../ui/search";
 import style from "./index.module.css";
 const HomePage = () => {
-  return <div className={style.container}></div>;
+  return <div className={style.container}>
+    <Search />
+  </div>;
 };
 
 export default HomePage;
