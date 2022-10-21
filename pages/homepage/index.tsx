176b4b0ffce946df17d1d5156c45f34903cd36cb
Alex Bal <alexenri12@gmail.com>
2022-10-21 17:32:19 -0300

    test-carrusel

--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -1,9 +1,11 @@
 import Search from "../../ui/search";
 import style from "./index.module.css";
 const HomePage = () => {
-  return <div className={style.container}>
-    <Search />
-  </div>;
+  return (
+    <div className={style.container}>
+      <Search />
+    </div>
+  );
 };
 
 export default HomePage;
