65e0f30d0d6cf24b363a90b5c9d723474a317f92
jcdavidarg <jcdavidarg@gmail.com>
2022-10-22 16:30:36 -0300

    fixed navbar created and added to pages

--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -1,11 +1,15 @@
 import Search from "../../ui/search";
 import style from "./index.module.css";
 import Categories from "../../ui/categories";
+import Navbar from "../../ui/navbar";
 const HomePage = () => {
-  return <div className={style.container}>
+  return (
+    <div className={style.container}>
       <Search />
       <Categories />
+      <Navbar page="Inicio" />
     </div>
+  );
 };
 
 export default HomePage;
---
+++ ui/navbar/index.tsx
@@ -0,0 +1,80 @@
+import { ReactElement } from "react";
+import Image from "next/image";
+import style from "./index.module.css";
+
+type navBarType = {
+  page: "Inicio" | "Buscar" | "Guardados" | "Galeria";
+};
+
+const Navbar = (props: navBarType): ReactElement => {
+  const { page } = props;
+
+  return (
+    <nav className={style.containerNav}>
+      <ul className={style.containerUl}>
+        {page === "Inicio" ? (
+          <li className={style.liContainer}>
+            <Image src="/images/home.svg" width={27} height={27} />
+            {page}
+          </li>
+        ) : (
+          <li className={style.liContainer}>
+            <Image
+              src="/images/home.svg"
+              width={27}
+              height={27}
+              className={style.iconImg}
+            />
+          </li>
+        )}
+        {page === "Buscar" ? (
+          <li className={style.liContainer}>
+            <Image src="/images/search.svg" width={27} height={27} />
+            {page}
+          </li>
+        ) : (
+          <li className={style.liContainer}>
+            <Image
+              src="/images/search.svg"
+              width={27}
+              height={27}
+              className={style.iconImg}
+            />
+          </li>
+        )}
+        {page === "Guardados" ? (
+          <li className={style.liContainer}>
+            <Image src="/images/saves.svg" width={27} height={27} />
+            {page}
+          </li>
+        ) : (
+          <li className={style.liContainer}>
+            <Image
+              src="/images/saves.svg"
+              width={27}
+              height={27}
+              className={style.iconImg}
+            />
+          </li>
+        )}
+        {page === "Galeria" ? (
+          <li className={style.liContainer}>
+            <Image src="/images/gallery.svg" width={27} height={27} />
+            {page}
+          </li>
+        ) : (
+          <li className={style.liContainer}>
+            <Image
+              src="/images/gallery.svg"
+              width={27}
+              height={27}
+              className={style.iconImg}
+            />
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
