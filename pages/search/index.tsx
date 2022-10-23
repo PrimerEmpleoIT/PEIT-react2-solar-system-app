8f9c0809a69feda4f7b52c6ee2e6554ced1fbb74
jcdavidarg <jcdavidarg@gmail.com>
2022-10-23 15:05:18 -0300

    headernav added to pages

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,19 @@
 import Navbar from "../../components/navbar";
 import style from "./index.module.css";
+import HeaderNav from "../../components/headernav";
+import Titulo from "../../ui/title";
+
 const SearchPage = () => {
   return (
     <div className={style.container}>
+      <HeaderNav page="search" />
+      <div className={style.tituloContainer}>
+        <Titulo
+          titulo="Resultados de la búsqueda"
+          color="white"
+          textAlign="left"
+        />
+      </div>
       <Navbar page="Buscar" />
     </div>
   );
