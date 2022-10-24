eefdf44416c729c099339f60e3a6c06cfe2fe9b6
jcdavidarg <jcdavidarg@gmail.com>
2022-10-24 14:41:35 -0300

    responsive design and added missings leyend tests boxes

--- pages/saved/index.tsx
+++ pages/saved/index.tsx
@@ -6,13 +6,13 @@ const SavedPage = () => {
   return (
     <div className={style.container}>
       <div className={style.containerComponents}>
-      <HeaderNav page={"no-title"} />
-      <Title titulo={"Guardados"} color={"white"} textAlign={"left"} />
-      {/*codigo de prueba */}
-      <h1 style={{color:"white"}}> Aquí va el componente Card</h1>
-      <h1 style={{color:"white"}}> Aquí va el componente Card</h1>
-      <h1 style={{color:"white"}}> Aquí va el componente Card</h1>
-      {/*fin codigo de prueba */}
+        <HeaderNav page={"no-title"} />
+        <Title titulo={"Guardados"} color={"white"} textAlign={"left"} />
+        {/*codigo de prueba */}
+        <h1 style={{ color: "white" }}> Aquí va el componente Card guardado</h1>
+        <h1 style={{ color: "white" }}> Aquí va el componente Card guardado</h1>
+        <h1 style={{ color: "white" }}> Aquí va el componente Card guardado</h1>
+        {/*fin codigo de prueba */}
       </div>
       <Navbar page={"Guardados"} />
     </div>
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -2,19 +2,29 @@ import Navbar from "../../components/navbar";
 import style from "./index.module.css";
 import HeaderNav from "../../components/headernav";
 import Titulo from "../../ui/title";
+import Search from "../../components/search";
 
 const SearchPage = () => {
   return (
     <div className={style.container}>
       <div className={style.containerComponents}>
-      <HeaderNav page="no-title" />
+        <HeaderNav page="no-title" />
         <Titulo
           titulo="Resultados de la búsqueda"
           color="white"
           textAlign="left"
         />
+        <Search />
+        {/*codigo de prueba */}
+        <h1 style={{ color: "white" }}>
+          Aquí va el componente Card buscado con su pre descripcion
+        </h1>
+        {/*fin codigo de prueba */}
+        <p className={style.interest}>También te puede interesar</p>
+        <h1 style={{ color: "white" }}>
+          Aquí va el ultimo componente Card guardado con su pre descripcion
+        </h1>
       </div>
-
       <Navbar page="Buscar" />
     </div>
   );
