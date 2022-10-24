ff2993b30319cf5a4859071300bf098973c0d19d
jcdavidarg <jcdavidarg@gmail.com>
2022-10-24 02:42:17 -0300

    card component modificated

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,22 +1,29 @@
-
 import style from "./card.module.css";
 import Image from "next/image";
+import Titulo from "../../ui/title";
 
 const Card = () => {
-
-        return (<div className={style.card_container}>
-                <Image src={"/neptuneCard.png"} alt="" width={165} height={160} />
-                <h1 className={style.card_title}>Netuno</h1>
-                <span className={style.saves}>
-                        <Image src={"/images/saves.svg"} width={22} height={22} />
-                </span>
-                <p className={style.continue}>Continue lendo</p>
-                <p className={style.paragraph}>Netuno é o oitavo planeta do Sistema Solar, o último a partir do Sol desde a reclassificação...</p>
-                <span className={style.arrowCard}>
-                        <Image src={"/arrow.svg"} width={16} height={16} />
-                </span>
+  return (
+    <div className={style.cardContainer}>
+      <div className={style.leftSide}>
+        <Image src={"/neptuneCard.png"} alt="" width={250} height={250} />
+      </div>
+      <div className={style.rightSide}>
+        <div className={style.title}>
+          <h1>Neptuno</h1>
+          <Image src={"/images/saves.svg"} width={15} height={15} />
         </div>
-        )
+        <p>
+          Neptuno es el octavo planeta del Sistema Solar, el último procedente
+          del Sol desde la reclasificación...
+        </p>
+        <div className={style.continue}>
+          <p>Continue leyendo</p>
+          <Image src={"/arrow.svg"} width={16} height={16} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
