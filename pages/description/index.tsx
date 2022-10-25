7c007386a17066a3a4828f4085a07b0fbbf373e7
jcdavidarg <jcdavidarg@gmail.com>
2022-10-25 05:27:23 -0300

    article component, page description and card element solved

--- pages/description/index.tsx
+++ pages/description/index.tsx
@@ -1,15 +1,19 @@
 import Article from "../../components/description";
 import style from "./index.module.css";
-import data from "./data"
+import data from "./data";
+import Navbar from "../../components/navbar";
+import HeaderNav from "../../components/headernav";
 
-export default function Description( ){
-    
-    return <div className={style.container}>
-        <Article 
-            name={data.name}
-            id={3}
-            data={data.data}
-        />
-
-    </div>;
+export default function Description() {
+  return (
+    <div className={style.container}>
+      <div className={style.position}>
+        <HeaderNav page="no-title" />
+      </div>
+      <div className={style.containerComponents}>
+        <Article name={data.name} id={3} data={data.data} imagen="save" />
+      </div>
+      <Navbar page="Buscar" />
+    </div>
+  );
 }
