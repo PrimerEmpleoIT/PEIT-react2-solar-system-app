22dab95f3ae15b08630a6e981dad11de2585bad6
Alex Bal <alexenri12@gmail.com>
2022-10-29 11:24:20 -0300

    header-nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import HomePage from "./homepage";
 import WelcomePage from "./welcome";
 const Home: NextPage = () => {
-  return <WelcomePage />;
+  const [ifName, setIfName] = useState(false);
+  useEffect(() => {
+    const fullname = sessionStorage.getItem("fullname")?.toString();
+    //Si existe "fullname" almacenado en sessionStorage mi flag es true
+    if (fullname != undefined) {
+      setIfName(true);
+    }
+    //Si no existe será siempre false y direcciona a WelcomePage
+  }, []);
+  return <>{ifName === true ? <HomePage /> : <WelcomePage />} ;</>;
 };
 
 export default Home;
