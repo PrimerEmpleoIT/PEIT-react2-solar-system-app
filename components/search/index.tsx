2ed099af83d25fbf21957df4013734cb6c9c9e7f
Elias Kanneman <elias_gk1@hotmail.com>
2022-10-31 01:39:50 -0300

    Search filter data v0.0

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,16 +1,45 @@
 import Image from "next/image";
+import React, { useState } from "react";
 import style from "./search.module.css";
+import data from "./Data.json"
+import Link from "next/link";
 
-export default function Search(props: any) {
+interface DataSearch{
+  id: number | string;
+  first_name: string;
+  image?: string;
+}
+
+
+export default function Search() {
+  const [filteredData, setFilteredData] =  useState<Array<DataSearch>>([])
+  const [search, setSearch] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  function handleSearch(e: React.ChangeEvent<HTMLInputElement>){
+    const searchWord = e.target.value
+    const newFilter = data.filter((value) => {
+      return value.first_name.toLowerCase().includes(searchWord)
+    });
+    if(searchWord === ""){
+      setFilteredData([])
+    }else {
+      setFilteredData(newFilter)
+    }
+    
+  }
+  
   return (
     <div className={style.box}>
+      
       <input
         autoComplete="off"
         spellCheck="false"
         className={style.search}
-        type="text"
+        type="search"
         name="keywords"
         placeholder="Buscar"
+        onChange={handleSearch}
       />
       <span className={style.btn_search}>
         <Image
@@ -20,6 +49,14 @@ export default function Search(props: any) {
           height={15}
         />
       </span>
+      { filteredData.length != 0 && (
+        <div className={style.dataResult}>
+          {filteredData.map((value) => {
+          return <a href="/gallery" key={value.id} className={style.dataItem}> <p>{value.first_name}</p></a>
+        })}
+        </div>
+      )}
+      
     </div>
   );
 }
