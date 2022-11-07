019ce809126d6efce652c8df484c26898309a128
jcdavidarg <jcdavidarg@gmail.com>
2022-11-07 15:59:00 -0300

    corrected css

--- components/menu-suggestions/index.tsx
+++ components/menu-suggestions/index.tsx
@@ -1,11 +1,9 @@
 import { ReactElement } from "react";
 import Suggestions from "../../ui/suggestions";
-import style from"./suggestions.module.css"
+import style from "./suggestions.module.css";
 const MenuSuggestions = (): ReactElement => {
   return (
-    <div
-      className={style.container}
-    >
+    <div className={style.container}>
       <Suggestions name="Sol" />
       <Suggestions name="Tierra" />
       <Suggestions name="Via Lactea" />
