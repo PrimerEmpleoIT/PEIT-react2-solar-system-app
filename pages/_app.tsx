4f3f098601b4d1072a936e3af91710aefebc05ce
Alex Bal <alexenri12@gmail.com>
2022-11-13 04:07:38 -0300

    add-background-particles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
-
+import BackgroundParticles from "../components/background";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -12,6 +12,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <meta name="description" content="" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <div className={"background-particles"}>
+          <BackgroundParticles />
+        </div>
         <Component {...pageProps} />
       </RecoilRoot>
     </>
