10c0a116195a2591726fbc26504fae8378ba7db0
Alex Bal <alexenri12@gmail.com>
2022-11-14 11:52:15 -0300

    deleted-consolelog

--- components/background/index.tsx
+++ components/background/index.tsx
@@ -1,30 +1,16 @@
-import type { Container, Engine } from "tsparticles-engine";
+import type { Engine } from "tsparticles-engine";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { useCallback } from "react";
 import configParticles from "./particles.json";
 const BackgroundParticles = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
-
-    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(engine);
   }, []);
-
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {
-      await console.log(container);
-    },
-    []
-  );
-
   return (
     <Particles
       id="tsparticles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={configParticles}
     />
   );
